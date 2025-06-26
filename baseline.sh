5ab50f94252deea0a2eb04f03b298824b59a70cd
Renaud Lepage <root@cybikbase.com>
2025-06-26 13:20:24 -0700

    adjustments

--- baseline.sh
+++ baseline.sh
@@ -2,4 +2,4 @@
 
 [ "$(id -u)" == "0" ] || { echo "Please run this script as root."; exit 1; }
 
-apt install -y librust-openssl-sys-dev
+apt install -y librust-openssl-sys-dev protobuf-codegen protobuf-c-compiler protobuf-compiler librust-js-sys-dev
